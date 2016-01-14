91db68c387185e4ab91119f8d4a01bab0da95793
Samuel Hubbard <hubbardsr@fullsail.edu>
2016-01-13 23:03:36 -0600

    Initial push.

---
+++ ReleaseDate/app/src/main/java/com/samuelhubbard/android/releasedate/TrackedGamesActivity.java
@@ -0,0 +1,13 @@
+package com.samuelhubbard.android.releasedate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrackedGamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracked_games);
+    }
+}
