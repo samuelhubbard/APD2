74dde23155648fafe3bbd41adebce49982dba241
Samuel Hubbard <hubbardsr@fullsail.edu>
2016-01-13 23:40:42 -0600

    Added the navigation from tracked games screen. Applied color scheme.

---
+++ ReleaseDate/app/src/main/java/com/samuelhubbard/android/releasedate/AboutAppActivity.java
@@ -0,0 +1,17 @@
+package com.samuelhubbard.android.releasedate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samuelhubbard on 1/13/16.
+ */
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- ReleaseDate/app/src/main/java/com/samuelhubbard/android/releasedate/TrackedGamesActivity.java
+++ ReleaseDate/app/src/main/java/com/samuelhubbard/android/releasedate/TrackedGamesActivity.java
@@ -1,7 +1,10 @@
 package com.samuelhubbard.android.releasedate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TrackedGamesActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class TrackedGamesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracked_games);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // pull which item was tapped
+        int id = item.getItemId();
+
+        //TODO: Add navigation to other activities
+        if (id == R.id.action_upcoming) {
+            Intent i = new Intent(this, UpcomingGamesActivity.class);
+            startActivityForResult(i, 0);
+            return true;
+        }
+
+        if (id == R.id.action_about) {
+            Intent i = new Intent(this, AboutAppActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
